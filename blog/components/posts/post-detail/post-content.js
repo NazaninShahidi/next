b45aeef4e2f9dfa4f09f94702af0c23162234adf
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-04-06 23:43:28 +0330

    edit post-content

--- blog/components/posts/post-detail/post-content.js
+++ blog/components/posts/post-detail/post-content.js
@@ -1,21 +1,53 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-const DUMMY_POST = {
-  title: "getting-started",
-  image: "getting-started-nextjs.png",
-  content: "# This is a first post",
-  date: "2022-02-10",
-  slug: "getting-started-4",
-};
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const customRenderes = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { language, value } = code;
+      return <Prism style={atomDark} language={language} children={value} />;
+    },
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown renderes={customRenderes}>{post.content}</ReactMarkdown>
     </article>
   );
 }

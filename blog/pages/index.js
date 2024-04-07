fb0fbc9a1ff308707eb1634dca9ab079fae930f3
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-04-07 21:28:55 +0330

    add head tag

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -2,10 +2,15 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="this is test" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
